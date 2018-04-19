9da2e3eef3aa89cb0aaaabe86f4be81e44e7d000
olf <olf@subsignal.org>
2018-04-19 20:25:10 +0200

    added preprocessing stadtarchiv

---
+++ README.md
@@ -0,0 +1,36 @@
+# damals
+
+A tour around
+ Leipzig based on historic photos.
+
+## Roadmap
+
+## Preprocessing
+
+### Prepare Stadtarchiv
+
+1. First we need to extract all information from the provived csv file, 
+   and convert it to a more convenient form (JSON).
+   
+   ```zsh
+   python preprocessing/preprocess_stadtarchiv.py 
+   ```
+   
+   Executing this script will create a json file for each photo in the collection,
+   and create a txt file with all urls.
+
+2. Fetch the images from the server. We could have used python-requests, 
+   but it's way more convenient to simply use wget and a for loop.
+   
+   ```zsh
+   cd rawdata/stadtatchiv
+   for url in `cat stadtarchiv_urls.txt`; do; wget ${url}l done 
+   ```
+ 
+ ### Prepare Stadtgeschichtliches Museum
+ 
+ 
+ 
+ # License
+ 
+ GNU General Public License v3
---
+++ preprocessing/preprocess_stadtarchiv.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+This script fetches the images of the dataset provided by Stadtarchiv Leipzig for Coding Da Vinci Ost 2018.
+"""
+
+import csv
+import json
+
+from os.path import dirname, abspath, join
+
+
+# CONSTANTS
+BASE_PATH = dirname(abspath(__file__))
+METADATA_FILE = join(BASE_PATH, "..", "rawdata", "stadtarchiv", "StadtAL_CodingDaVinci.csv")
+OUT_PATH = join(BASE_PATH, "..", "rawdata", "stadtarchiv")
+FETCH_URLS_FILE = join(BASE_PATH, "..", "rawdata", "stadtarchiv", "stadtarchiv_urls.txt")
+
+# We define the header manually because there are strange symbols in the csv file :/
+HEADER = ['Archivsignatur',
+          'Bezeichnung',
+          'Ort',
+          'Gebäude',
+          'Straßen',
+          'Fotograf',
+          'ergänzende Datierung',
+          'Datierung',
+          'Copyright',
+          'Lizenz',
+          'URL Datensatz',
+          'URL Datei']
+
+def get_metadata(metadata_file=METADATA_FILE):
+    with open(metadata_file) as mfile:
+        meta_reader = csv.reader(mfile, delimiter=";")
+        # skip the header
+        next(meta_reader)
+        for row in meta_reader:
+            yield dict(zip(HEADER, row))
+
+
+def save_metadata_and_write_urls(fetch_urls_file=FETCH_URLS_FILE):
+    fetch_urls = []
+    for metadata in get_metadata():
+
+        fetch_url = metadata["URL Datei"]
+        meta_filename = fetch_url.split('/')[-1].split(".")[0]+".json"
+
+        with open(join(OUT_PATH, meta_filename), "w") as mfile:
+            json.dump(metadata, mfile)
+
+        fetch_urls.append(fetch_url)
+
+    with open(fetch_urls_file, "w") as fetch_file:
+        for fetch_url in fetch_urls:
+            fetch_file.write(fetch_url+"\n")
+
+def main():
+    save_metadata_and_write_urls()
+
+if __name__ == "__main__":
+    main()
